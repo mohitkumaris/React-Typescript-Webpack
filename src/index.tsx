d702bf02250854240fb0e2174ead217936d6a588
Mohit Kumar <mohitkumaris@gmail.com>
2023-07-17 14:57:20 +0530

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { App } from "./app";
+
+ReactDOM.render(<App num={1234} />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      // `ts` and `tsx` files are parsed using `ts-loader`
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
